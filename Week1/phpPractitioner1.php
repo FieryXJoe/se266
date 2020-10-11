a01992809ae68bf22f0d223ffb53772e45a06738
FieryXJoe <josephmsherry@comcast.net>
2020-10-11 15:09:39 -0400

    PHP Practitioner Assignment working, no changes to home page made

---
+++ Week1/phpPractitioner1.php
@@ -0,0 +1,10 @@
+<?php
+    $animals = [
+        "Cow",
+        "Horse",
+        "Chicken",
+        "Dog",
+        "Cat"
+    ];
+
+    require 'phpPractitioner1.view.php';
---
+++ Week1/phpPractitioner1.view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<ul>
+    <?php
+        foreach($animals as $animal)
+            echo '<li>' . $animal . "</li>";
+    ?> 
+</ul>
+
+</body>
+</html>
