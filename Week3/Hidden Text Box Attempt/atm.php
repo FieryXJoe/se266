143d5bbab4486a22bdf911c97c42298b01161ff5
FieryXJoe <josephmsherry@comcast.net>
2020-10-22 14:11:31 -0400

    hidden textbox version now working

--- Week3/Hidden Text Box Attempt/atm.php
+++ Week3/Hidden Text Box Attempt/atm.php
@@ -1,26 +1,18 @@
 <?php
-    $checkingBal = isset($_POST["chckWithdrawTxt"]) || isset($_POST["chckDepositTxt"]) ? filter_input (INPUT_POST, 'checkingHidden') : 1000.00;
-    $savingsBal = isset($_POST["savingWithdrawTxt"]) || isset($_POST["savingDepositTxt"]) ? filter_input (INPUT_POST, 'savingsHidden') : 5000.00;
-    
-    /*In 100% of cases these values are null, even if I rearrange the code
-    even if I use INPUT_GET instead of INPUT_POST*/
-    var_dump(filter_input (INPUT_GET, 'checkingHidden'));
-    var_dump(filter_input (INPUT_POST, 'savingsHidden'));
-    
+    $checkingBal = filter_input (INPUT_POST, 'checkingHidden') != NULL ? filter_input (INPUT_POST, 'checkingHidden') : 1000.00;
+    $savingsBal = filter_input (INPUT_POST, 'savingsHidden')  != NULL ? filter_input (INPUT_POST, 'savingsHidden') : 5000.00;
+
     if(count($_POST)!=0)
     {
-        if(isset($_POST["chckWithdrawTxt"]))
+        if(isset($_POST["chckWithdrawTxt"]) && $_POST["chckWithdrawTxt"] != "")
             $checkingBal -= $_POST["chckWithdrawTxt"];
-        if(isset($_POST["chckDepositTxt"]))
+        if(isset($_POST["chckDepositTxt"])  && $_POST["chckDepositTxt"] != "")
             $checkingBal += $_POST["chckDepositTxt"];
-        if(isset($_POST["savingWithdrawTxt"]))
+        if(isset($_POST["savingWithdrawTxt"]) && $_POST["savingWithdrawTxt"] != "")
             $savingsBal -= $_POST["savingWithdrawTxt"];
-        if(isset($_POST["savingDepositTxt"]))
+        if(isset($_POST["savingDepositTxt"]) && $_POST["savingDepositTxt"] != "")
             $savingsBal += $_POST["savingDepositTxt"];
     }
 
     require 'atm.view.php';
-
-    var_dump(filter_input (INPUT_POST, 'checkingHidden'));
-    var_dump(filter_input (INPUT_GET, 'savingsHidden'));
     
--- Week3/Hidden Text Box Attempt/atm.view.php
+++ Week3/Hidden Text Box Attempt/atm.view.php
@@ -20,46 +20,40 @@
 </head>
 <body>
     <h1>ATM</h1>
-    <table id="accounts">
-        <tr>
-            <td>
-                <h2>Checking Account</h2>
-                <ul>
-                    <li>Account ID: C123</li>
-                    <li>Balance: $<?=number_format($checkingBal , 2)?></li>
-                    <li>Account Opened: 12-20-2019</li>
-                </ul>
-                <form id="chckWithdraw" method="post">
-                    <input type="text" id="chckWithdrawTxt" name="chckWithdrawTxt">
-                    <input type="submit" value="Withdraw" id="chckWithdrawBtn">
-                </form>
-                <form id="chckDeposit" method="post">
-                    <input type="text" id="chckDepositTxt" name="chckDepositTxt">
-                    <input type="submit" value="Deposit" id="chckDepositBtn">
-                </form>
-            </td>
-            <td>
-                <h2>Savings Account</h2>
-                <ul>
-                    <li>Account ID: S123</li>
-                    <li>Balance: $<?=number_format($savingsBal , 2)?></li>
-                    <li>Account Opened: 03-20-2020</li>
-                </ul>
-                <form id="savingWithdraw" method="post">
-                    <input type="text" id="savingWithdrawTxt" name="savingWithdrawTxt">
-                    <input type="submit" value="Withdraw" id="savingWithdrawBtn">
-                </form>
-                <form id="savingDeposit" method="post">
-                    <input type="text" id="savingDepositTxt" name="savingDepositTxt">
-                    <input type="submit" value="Deposit" id="savingDepositBtn">
-                </form>
-            </td>
-        </tr>
-    </table>
-    <!--Put these in their own from because other options are put a copy in every form or pick one at random to put them in-->
     <form method="post">
-        <!--Will hide these once everything is working-->
-        <input type="text" id="checkingHidden" name="checkingHidden" value = "<?=$checkingBal?>" />
-        <input type="text" id="savingsHidden" name="savingsHidden" value ="<?=$savingsBal?>" />
+        <table id="accounts">
+            <tr>
+                <td>
+                    <h2>Checking Account</h2>
+                    <ul>
+                        <li>Account ID: C123</li>
+                        <li>Balance: $<?=number_format($checkingBal , 2)?></li>
+                        <li>Account Opened: 12-20-2019</li>
+                    </ul>
+                        <input type="text" id="chckWithdrawTxt" name="chckWithdrawTxt">
+                        <input type="submit" value="Withdraw" id="chckWithdrawBtn">
+                        <br />
+                        <input type="text" id="chckDepositTxt" name="chckDepositTxt">
+                        <input type="submit" value="Deposit" id="chckDepositBtn">
+                </td>
+                <td>
+                    <h2>Savings Account</h2>
+                    <ul>
+                        <li>Account ID: S123</li>
+                        <li>Balance: $<?=number_format($savingsBal , 2)?></li>
+                        <li>Account Opened: 03-20-2020</li>
+                    </ul>
+                        <input type="text" id="savingWithdrawTxt" name="savingWithdrawTxt">
+                        <input type="submit" value="Withdraw" id="savingWithdrawBtn">
+                        <br />
+                        <input type="text" id="savingDepositTxt" name="savingDepositTxt">
+                        <input type="submit" value="Deposit" id="savingDepositBtn">
+                </td>
+            </tr>
+        </table>
+        <!--Put these in their own from because other options are put a copy in every form or pick one at random to put them in-->
+            <!--Will hide these once everything is working-->
+            <input type="text" id="checkingHidden" name="checkingHidden" value = "<?=$checkingBal?>" style = "display:none" />
+            <input type="text" id="savingsHidden" name="savingsHidden" value ="<?=$savingsBal?>" style = "display:none" />
     </form>
 </body>
