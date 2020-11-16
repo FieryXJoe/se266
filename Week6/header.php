3725191aa572b41fbcf56a3065fae3e6285a3e2d
FieryXJoe <josephmsherry@comcast.net>
2020-11-16 00:14:43 -0500

    fixed broken link

--- Week6/header.php
+++ Week6/header.php
@@ -4,5 +4,5 @@
     <li><a href="http://se266-sherry-j-2020.herokuapp.com/">Home</a></li>
     <li><a href="http://se266-sherry-j-2020.herokuapp.com/Week6/upload.php">Upload</a></li>
     <li><a href="http://se266-sherry-j-2020.herokuapp.com/Week6/search.php">Search</a></li>
-    <li style="float:right"><a href="http://se266-sherry-j-2020.herokuapp.com/Week6/upload.php">Logout</a></li>
+    <li style="float:right"><a href="http://se266-sherry-j-2020.herokuapp.com/Week6/logoff.php">Logout</a></li>
 </ul>
